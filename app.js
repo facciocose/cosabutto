847d21b835a83db23c6e2f24d0e8f8de97845acc
Luca Marra <ldm.marra@gmail.com>
2017-05-04 17:28:44 +0200

    separate db file

--- app.js
+++ app.js
@@ -1,49 +1,5 @@
 var comune = "CT";
 
-var DB = {
-  CT: {
-    LABEL: "Catania",
-    RIFIUTO: {
-      INDIFFERENZIATO: {
-        COLOR: "#455660",
-        TEXT_COLOR: "#fff",
-        LABEL: "Indifferenziato"
-      },
-      ORGANICO: {
-        COLOR: "#7E543B",
-        TEXT_COLOR: "#fff",
-        LABEL: "Organico"
-      },
-      CARTA: {
-        COLOR: "#016AAB",
-        TEXT_COLOR: "#fff",
-        LABEL: "Carta"
-      },
-      PLASTICA: {
-        COLOR: "#E9C74B",
-        TEXT_COLOR: "#000",
-        LABEL: "Plastica"
-      },
-      VETRO: {
-        COLOR: "#4E6F16",
-        TEXT_COLOR: "#fff",
-        LABEL: "Vetro"
-      }
-    }
-  }
-};
-
-DB.CT.GIORNO = {
-  DOMENICA: DB.CT.RIFIUTO.VETRO,
-  LUNEDI: DB.CT.RIFIUTO.INDIFFERENZIATO,
-  MARTEDI: DB.CT.RIFIUTO.ORGANICO,
-  MERCOLEDI: DB.CT.RIFIUTO.CARTA,
-  GIOVEDI: DB.CT.RIFIUTO.ORGANICO,
-  VENERDI: DB.CT.RIFIUTO.PLASTICA,
-  SABATO: DB.CT.RIFIUTO.ORGANICO
-};
-
-
 $(function() {
   var $title = $("#title");
   var $subtitle = $("#subtitle");
---
+++ db.js
@@ -0,0 +1,42 @@
+var DB = {
+  CT: {
+    LABEL: "Catania",
+    RIFIUTO: {
+      INDIFFERENZIATO: {
+        COLOR: "#455660",
+        TEXT_COLOR: "#fff",
+        LABEL: "Indifferenziato"
+      },
+      ORGANICO: {
+        COLOR: "#7E543B",
+        TEXT_COLOR: "#fff",
+        LABEL: "Organico"
+      },
+      CARTA: {
+        COLOR: "#016AAB",
+        TEXT_COLOR: "#fff",
+        LABEL: "Carta"
+      },
+      PLASTICA: {
+        COLOR: "#E9C74B",
+        TEXT_COLOR: "#000",
+        LABEL: "Plastica"
+      },
+      VETRO: {
+        COLOR: "#4E6F16",
+        TEXT_COLOR: "#fff",
+        LABEL: "Vetro"
+      }
+    }
+  }
+};
+
+DB.CT.GIORNO = {
+  DOMENICA: DB.CT.RIFIUTO.VETRO,
+  LUNEDI: DB.CT.RIFIUTO.INDIFFERENZIATO,
+  MARTEDI: DB.CT.RIFIUTO.ORGANICO,
+  MERCOLEDI: DB.CT.RIFIUTO.CARTA,
+  GIOVEDI: DB.CT.RIFIUTO.ORGANICO,
+  VENERDI: DB.CT.RIFIUTO.PLASTICA,
+  SABATO: DB.CT.RIFIUTO.ORGANICO
+};
