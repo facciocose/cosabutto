3d3ab99ec2dff90e3312d197d389b9238ef77dfd
Luca Marra <ldm.marra@gmail.com>
2017-05-05 09:53:39 +0200

    minor refactoring

--- db.js
+++ db.js
@@ -1,32 +1,33 @@
-var DB = {
-  CT: {
-    LABEL: "Catania",
-    RIFIUTO: {
-      INDIFFERENZIATO: {
-        COLOR: "#455660",
-        TEXT_COLOR: "#fff",
-        LABEL: "Indifferenziato"
-      },
-      ORGANICO: {
-        COLOR: "#7E543B",
-        TEXT_COLOR: "#fff",
-        LABEL: "Organico"
-      },
-      CARTA: {
-        COLOR: "#016AAB",
-        TEXT_COLOR: "#fff",
-        LABEL: "Carta"
-      },
-      PLASTICA: {
-        COLOR: "#E9C74B",
-        TEXT_COLOR: "#000",
-        LABEL: "Plastica"
-      },
-      VETRO: {
-        COLOR: "#4E6F16",
-        TEXT_COLOR: "#fff",
-        LABEL: "Vetro"
-      }
+var DB = {};
+
+// Catania
+DB.CT = {
+  LABEL: "Catania",
+  RIFIUTO: {
+    INDIFFERENZIATO: {
+      COLOR: "#455660",
+      TEXT_COLOR: "#fff",
+      LABEL: "Indifferenziato"
+    },
+    ORGANICO: {
+      COLOR: "#7E543B",
+      TEXT_COLOR: "#fff",
+      LABEL: "Organico"
+    },
+    CARTA: {
+      COLOR: "#016AAB",
+      TEXT_COLOR: "#fff",
+      LABEL: "Carta"
+    },
+    PLASTICA: {
+      COLOR: "#E9C74B",
+      TEXT_COLOR: "#000",
+      LABEL: "Plastica"
+    },
+    VETRO: {
+      COLOR: "#4E6F16",
+      TEXT_COLOR: "#fff",
+      LABEL: "Vetro"
     }
   }
 };
